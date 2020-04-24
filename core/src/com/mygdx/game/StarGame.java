3fe6c4413cdb3c7efd9fc4393e54a12365b90581
Pavel <redbear95@gmail.com>
2020-04-24 19:55:14 +0300

    HomeWork1

--- core/src/com/mygdx/game/StarGame.java
+++ core/src/com/mygdx/game/StarGame.java
@@ -10,13 +10,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 50,50,250,250);
+		img = new Texture("space.jpg");
 	}
 
 	@Override
@@ -24,8 +22,7 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.5f, 0.6f, 0.7f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 150, 150, 100, 100);
-		batch.draw(region, 0,0, 100,100);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
